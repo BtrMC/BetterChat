2a7c649463e993e0d45454431bf84c03ba281d1d
razod <masonboulier@gmail.com>
2020-04-02 17:14:29 -0400

    add credit to main command

--- src/main/java/com/nexh/cmds/bc.java
+++ src/main/java/com/nexh/cmds/bc.java
@@ -14,12 +14,12 @@ public class bc implements CommandExecutor {
         Player p = (Player) sender;
         if(!p.hasPermission("bc.reload")) {Util.sendPl(p, Util.cColor("&cYou don't have permission!")); return false;}
         if(args.length == 0) {
-            Util.sendPl(p, Util.cColor("&3----------------------------------"));
+            Util.sendPl(p, Util.cColor("&3------ &3&lBetterChat &7by Nexh &3------"));
             Util.sendPl(p, Util.cColor("&3/clearchat &8- &7Clear the chat"));
             Util.sendPl(p, Util.cColor("&3/mutechat &8- &7Mute server chat"));
             Util.sendPl(p, Util.cColor("&3/announce &8- &7Announce to the server!"));
             Util.sendPl(p, Util.cColor("&3/bc rl &8- &7Reload the config"));
-            Util.sendPl(p, Util.cColor("&3----------------------------------"));
+            Util.sendPl(p, Util.cColor("&3------ &3&lBetterChat &7by Nexh &3------"));
         } else if(args[0].equalsIgnoreCase("rl")) {
             plugin.reloadConfig();
             Util.sendPl(p, Util.cColor("&cReloaded the config!"));
@@ -27,12 +27,12 @@ public class bc implements CommandExecutor {
             plugin.reloadConfig();
             Util.sendPl(p, Util.cColor("&cReloaded the config!"));
         } else {
-            Util.sendPl(p, Util.cColor("&3----------------------------------"));
+            Util.sendPl(p, Util.cColor("&3------ &3&lBetterChat &7by Nexh &3------"));
             Util.sendPl(p, Util.cColor("&3/clearchat &8- &7Clear the chat"));
             Util.sendPl(p, Util.cColor("&3/mutechat &8- &7Mute global chat"));
             Util.sendPl(p, Util.cColor("&3/announce &8- &7Announce to the server!"));
             Util.sendPl(p, Util.cColor("&3/bc rl &8- &7Reload the config"));
-            Util.sendPl(p, Util.cColor("&3----------------------------------"));
+            Util.sendPl(p, Util.cColor("&3------ &3&lBetterChat &7by Nexh &3------"));
         }
         return true;
     }
