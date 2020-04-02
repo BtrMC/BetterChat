2f366530432df7b644900d0a90ceb05a73d8148c
razod <masonboulier@gmail.com>
2020-04-02 17:19:27 -0400

    optimize

--- src/main/java/com/nexh/Util.java
+++ src/main/java/com/nexh/Util.java
@@ -5,13 +5,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public class Util {
-        public static String cColor(String textToTranslate) {
-            return ChatColor.translateAlternateColorCodes('&', textToTranslate);
-        }
+        public static String cColor(String textToTranslate) { return ChatColor.translateAlternateColorCodes('&', textToTranslate); }
         public static void sendPl(Player player, String msg) {
             player.sendMessage(msg);
         }
-        public static void bcMsg(String msg) {
-            Bukkit.broadcastMessage(msg);
-        }
+        public static void bcMsg(String msg) { Bukkit.broadcastMessage(msg); }
 }
