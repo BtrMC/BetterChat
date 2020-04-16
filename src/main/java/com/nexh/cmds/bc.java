82d9c18ce9a7034641c3ccdfa103054aad5e2cd2
razod <masonboulier@gmail.com>
2020-04-16 14:11:23 -0400

    make version easier to edit

--- src/main/java/com/nexh/cmds/bc.java
+++ src/main/java/com/nexh/cmds/bc.java
@@ -8,12 +8,13 @@ import org.bukkit.entity.Player;
 import com.nexh.Util;
 
 public class bc implements CommandExecutor {
+    private double version = 1.2;
     private Main plugin = Main.getPlugin(Main.class);
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
         Player p = (Player) sender;
         if(args.length == 0) {
-            Util.sendPl(p, Util.cColor("&3------ &3&lBetterChat v1.2 &7by Nexh &3------"));
+            Util.sendPl(p, Util.cColor("&3------ &3&lBetterChat v" + version + "&7by Nexh &3------"));
             Util.sendPl(p, Util.cColor("&3/clearchat &8- &7Clear the chat"));
             Util.sendPl(p, Util.cColor("&3/mutechat &8- &7Mute server chat"));
             Util.sendPl(p, Util.cColor("&3/announce &8- &7Announce to the server!"));
